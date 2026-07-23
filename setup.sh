2db43c42dd5edb2981abacbce49b57dcf8ce872e
mikufan12345 <298212556+mikufan12345@users.noreply.github.com>
2026-07-23 13:52:27 +0800

    aaaaaaaaaaaa

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "If any errors occur, please run as root."
-cp ./udp-server.service /etc/system/systemd/udp-server.service
+cp ./udp-server.service /etc/systemd/system/udp-server.service
 systemctl enable udp-server
 systemctl start udp-server
 echo "Startup success".
