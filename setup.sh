99a0fb3012c90577892063e77df300fb3190d141
mikufan12345 <298212556+mikufan12345@users.noreply.github.com>
2026-07-23 13:49:06 +0800

    add setup utility

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+echo "If any errors occur, please run as root."
+cp ./udp-server.service /etc/system/systemd/udp-server.service
+systemctl enable udp-server
+systemctl start udp-server
+echo "Startup success".
