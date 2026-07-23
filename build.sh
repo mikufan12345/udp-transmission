39d9fcc1ea3b60debd6a0a0d62b6e4fc0d2c7ab4
mikufan12345 <298212556+mikufan12345@users.noreply.github.com>
2026-07-23 17:12:29 +0800

    patch script errors

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [ ! -d "./build" ]; then
 	# make build directory
 	mkdir build
@@ -5,9 +6,10 @@ fi
 cd build && cmake .. && make
 
 # restart the service (only works when running as root)
-if [ "$EUID" -ne 0 ]; then 
+if [ "$(id -u)" -ne 0 ]; then 
     echo "Detected not running as root, elevating via sudo..."
 	echo "Ctrl+C to exit if you don't need to restart the server."
+	cd ..
     exec sudo "$0" "$@"
 fi
 systemctl stop udp-server
